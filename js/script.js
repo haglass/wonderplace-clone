408e1d5be4dc62caff0360eb893496abe5ebeca4
haglass <haglass@hanmail.net>
2023-03-27 20:09:18 +0900

    비주얼 슬라이드 작업중

--- js/script.js
+++ js/script.js
@@ -4,13 +4,12 @@ window.onload = function () {
   const mbWrap = document.querySelector(".mb-wrap");
   navMb.addEventListener("click", function () {
     // 인라인 방식
-    // mbWrap.style.display = "block";
-
     // if (mbWrap.style.display === "block") {
     //   mbWrap.style.display = "none";
     // } else {
     //   mbWrap.style.display = "block";
     // }
+
     // 클래스 추가 방식
     const state = mbWrap.classList.contains("active");
     if (state) {
@@ -20,10 +19,25 @@ window.onload = function () {
       navMb.classList.add("active");
       mbWrap.classList.add("active");
     }
-    // mbWrap.classList.add("active");
   });
-  // 비주얼슬라이드
-  new Swiper(".swVisual", {
+
+  // 비주얼 슬라이드
+  // 동적 즉, 서버에서 슬라이드를 생성하는 경우
+  const slideTotal = document.querySelectorAll(
+    ".swVisual .swiper-slide"
+  ).length;
+  const slideControlUl = document.querySelector(".sw-visual-control");
+  let html = "";
+  for (let i = 0; i < slideTotal; i++) {
+    let tempI = i + 1;
+    if (i < 9) {
+      tempI = "0" + (i + 1);
+    }
+    html += `<li>${tempI}</li>`;
+  }
+  slideControlUl.innerHTML = html;
+
+  const swVisual = new Swiper(".swVisual", {
     loop: true,
     speed: 800,
     autoplay: {
@@ -31,11 +45,46 @@ window.onload = function () {
       disableOnInteraction: false,
     },
     navigation: {
-      // css에 맞게 이름 변경
       nextEl: ".sw-visual-next",
       prevEl: ".sw-visual-prev",
     },
   });
 
-  //
+  // swVisual.on("slideChange", function () {
+  //   console.log("slide changed");
+  // });
+  swVisual.on("slideChangeTransitionStart", function () {
+    // console.log("slide changed", this.realIndex);
+    changeSwVisual(this.realIndex);
+  });
+
+  // 비주얼 슬라이드 제어 목록
+  let swVisualControlLi = document.querySelectorAll(".sw-visual-control li");
+  // console.log(swVisualControlLi);
+  swVisualControlLi.forEach((item, index) => {
+    // console.log(item);
+    item.addEventListener("click", function () {
+      // 내가 만드는 함수
+      changeSwVisual(index);
+    });
+  });
+
+  function changeSwVisual(_idx) {
+    // css 처리 (포커스 클래스 추가 및 제거)
+    // 1. active 클래스 모두 제거
+    swVisualControlLi.forEach((item, index) => {
+      item.classList.remove("active");
+    });
+    // 2. 클릭된 번호를 이용해서 클래스 추가하기
+    swVisualControlLi[_idx].classList.add("active");
+    // 3. 클릭된 번호를 이용해서 Swiper 를 이동
+    // * 주의 사항 (loop 를 옵션으로 준 경우는 아래 구문 금지)
+    // swVisual.slideTo(_index);
+    swVisual.slideToLoop(_idx, 500);
+  }
+
+  // 최초 클릭된 것처럼  active 클래스 추가
+  if (slideTotal > 0) {
+    changeSwVisual(0);
+  }
 };
